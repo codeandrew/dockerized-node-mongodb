73513765db8271b6b1a5ff38bc231d92d51c3b1c
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2018-12-16 18:00:16 +0800

    Initial Commit

---
+++ node/routes/questions.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+
+const mongodb = require('mongodb');
+const client = mongodb.MongoClient;
+
+const uri = "mongodb://mongo/petpal";
+
+router.get('/', function(req, res, next) {
+	client.connect(uri, function (err, db) {
+	    if (err) throw err;
+		const dbo = db.db();
+
+        res.send('Connected to MongoDB')
+	});
+});
+
+router.post('/add', function(req, res, next) {
+	client.connect(uri, function (err, db) {
+	    if (err) throw err;
+		const dbo = db.db();
+
+		try {
+			dbo.collection('questions', (err, collection) => {
+				collection.insert(req.body);
+
+				dbo.collection('questions').count((err,count) => {
+					if (err) throw err;
+					res.json(
+						{ "total_rows":  count }
+					)
+				})
+				db.close();
+			})
+		}
+		catch(error){
+			console.error(error)
+		}
+
+	});
+});
+
+router.get('/get', (req, res, next) => {
+	client.connect(uri, (err, db) => {
+		if (err) throw err
+		const dbo = db.db()
+
+		try {
+			dbo.collection('questions').find({}).toArray((err, result) =>{
+				if (err) throw err
+				res.json(result)
+				db.close()
+			})
+		}
+		catch(error){
+			console.error(error)
+		}
+	})
+})
+
+
+module.exports = router;
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Dockerized NodeJs and MongoDB
+> Dockerized node.js, express and mongodn using alpine linux
+
+## Setup
+> If you are in mac MongoDB volumes must be created outside the
+> User Directory
+
+Create a new Directory then point it in the docker-compose
+```
+ ~/Mongo/data/{PROJECT_NAME}
+```
+
+## Run
+```
+docker-compose build
+docker-compose up
+```
